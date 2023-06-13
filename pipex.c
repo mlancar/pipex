df577c3ec3195f7a5271e644525f24a5c6bdb1ba
Marine LANCAR <malancar@paul-f4Ar1s4.clusters.42paris.fr>
2023-06-13 20:08:17 +0200

    infile/outfile

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: malancar <malancar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 16:27:53 by malancar          #+#    #+#             */
-/*   Updated: 2023/06/12 20:18:29 by malancar         ###   ########.fr       */
+/*   Updated: 2023/06/13 19:23:54 by malancar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	check_access(char *cmd, char *path, char **cmd_path)
 	while (split_path[i])
 	{
 		*cmd_path = ft_strjoin(split_path[i], cmd, '/');
-		printf("cmd_path = %s\n", *cmd_path);
+		//printf("cmd_path = %s\n", *cmd_path);
 		if (access(*cmd_path, F_OK) == 0)
 			return (1);
 		free(*cmd_path);
@@ -36,16 +36,29 @@ int	check_access(char *cmd, char *path, char **cmd_path)
 	return (0);
 }
 
+/*void	pipex(int f1, int f2)
+{
+	int end[2];
+
+	pipe(end);
+}*/
+
+/*void	exec_cmd1(int fd, char *valid_cmd_path)
+{
+	
+	execve(const char *fichier, char *const argv[], char *const envp[]);
+}*/
+
 int	main(int ac, char **av, char **envp)
 {
 	int		i;
 	char	**cmd;
-	char	*cmd_path_valid;
+	char	*valid_cmd_path;
 
 	if (ac < 2)
 		return (0);
 	i = 0;
-	cmd = ft_split(av[1], ' ');
+	cmd = ft_split(av[2], ' ');
 	if (!cmd)
 		return (1);
 	while (envp[i])
@@ -54,13 +67,22 @@ int	main(int ac, char **av, char **envp)
 			break ;
 		i++;
 	}
-	if (check_access(cmd[0], &envp[i][5], &cmd_path_valid) == 0)
+	if (check_access(cmd[0], &envp[i][5], &valid_cmd_path) == 0)
 	{
 		free_tab(cmd);
 		printf("error");
 		return (1);//127 command not found
 	}
-	execve(cmd_path_valid, cmd, envp);
+	int infile = open(av[1], O_CREAT);
+	int outfile = open(av[3], O_CREAT);
+	//int infile = open(av[1], O_RDONLY);
+	//int outfile = open(av[1], O_RDONLY);
+	printf("outfile = %d\n", infile);
+	printf("infile = %d\n", outfile);
+	close(infile);
+	close(outfile);
+	//printf("fd = %d\n", infile);
+	execve(valid_cmd_path, cmd, envp);
 	//si execve reussi ne revient pas ici, sinon il faut free
 
 }
--- test.c
+++ test.c
@@ -1,20 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: malancar <malancar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/12 19:47:18 by malancar          #+#    #+#             */
-/*   Updated: 2023/06/13 18:47:13 by malancar         ###   ########.fr       */
+/*   Created: 2023/06/13 19:24:28 by malancar          #+#    #+#             */
+/*   Updated: 2023/06/13 20:07:11 by malancar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#ifndef PIPEX_H
-# define PIPEX_H
-//a utiliser pour l'index du pipe :
-# define WRITE 1
-# define READ 0
 
 # include <stdlib.h>
 # include <stdio.h>
@@ -24,9 +18,32 @@
 # include <sys/stat.h>
 # include <fcntl.h>
 
-int		check_access(char *cmd, char *path, char **cmd_path);
-char	**ft_split(char *str, char c);
-char	*ft_strjoin(char *s1, char *s2, char c);
-void	free_tab(char **tab);
+int main(int ac, char **av)
+{
+	int	infile;
+	int	outfile;
 
-#endif
+	if (ac < 2)
+		return (0);
+	infile = open(av[1], O_RDONLY);
+	if (infile == -1)
+	{
+		close(infile);
+		return (0);
+	}
+	outfile = open(av[2], O_WRONLY);
+	if (outfile == -1)
+	{
+		outfile = open(av[2], O_CREAT, S_IWUSR);
+		if (outfile == 1)
+		{
+			close(outfile);
+			return (0);
+		}
+	}
+	printf("infile = %d\n", infile);
+	printf("outfile = %d\n", outfile);
+	close(infile);
+	close(outfile);
+
+}
