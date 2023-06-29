9972725ac5016965390c42614ad767f5743d1a29
Marine LANCAR <malancar@paul-f4Ar6s7.clusters.42paris.fr>
2023-06-29 15:24:18 +0200

    bonus

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: malancar <malancar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 16:27:53 by malancar          #+#    #+#             */
-/*   Updated: 2023/06/29 13:03:09 by malancar         ###   ########.fr       */
+/*   Updated: 2023/06/29 15:22:25 by malancar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int	first_cmd(t_pipex *cmd, char **envp)
 		close(cmd->fd[0]);
 		execve(cmd->path, cmd->name, envp);
 		//free
+		//exit
 		return (dprintf(2, "execve marche pas :(\n"), 0);
 	}
 	else
@@ -44,7 +45,7 @@ int	middle_cmd(t_pipex *cmd, char **envp)
 	{
 		dup2(cmd->previous_fd, 0);
 		dup2(cmd->fd[1], 1);
-		//close(cmd->fd[0]);
+		close(cmd->fd[0]);
 		execve(cmd->path, cmd->name, envp);
 		//free
 		//exit
@@ -68,9 +69,9 @@ int	last_cmd(t_pipex *cmd, char **envp)
 		//dprintf(2, "coucou last cmd\n");
 		dup2(cmd->fd[0], 0);
 		dup2(cmd->outfile, 1);
-		close(cmd->fd[1]);
 		execve(cmd->path, cmd->name, envp);
 		//free
+		//exit
 		return (dprintf(2, "execve marche pas :(\n"), 0);
 	}
 	else
@@ -81,29 +82,46 @@ int	last_cmd(t_pipex *cmd, char **envp)
 	return (1);
 }
 
-int	pipex(char *first_arg, char *last_arg, t_pipex *cmd,char **envp)
+void	pipex(char *first_arg, char *last_arg, t_pipex *cmd,char **envp)
 {
-	if (cmd->index == cmd->first)
+	while (cmd.index > 0)
 	{
+		if (check_command(av[i], envp, &cmd) == 0)
+		{
+			//si cmd invalid faire le reste
+			
+		}
+		if (cmd->index == cmd->first)
+		{
 
-		pipe(cmd->fd);
-		if (first_cmd(cmd, envp) == 0)
-			return (0);
+			pipe(cmd->fd);
+			//protege si pipe echoue
+			//close free exit
+			if (first_cmd(cmd, envp) == 0)
+				return (0);
+		}
+		else if (cmd->index == cmd->last)
+		{
+			if (last_cmd(cmd, envp) == 0)
+				return (0);
+		}
+		else
+		{
+			cmd->previous_fd = cmd->fd[0];
+			pipe(cmd->fd);
+			if (middle_cmd(cmd, envp) == 0)
+				return (0);
+		}
+		//dprintf(2, "cmd.nbr = %d\n", cmd.nbr);
+		//dprintf(2, "cmd.index = %d\n", cmd.index);
+		//dprintf(2, "i = %d\n", i);
+		//dprintf(2, "cmd path = %s\ncmd name = %s\n", cmd.path, cmd.name[0]);
+		//dprintf(2, "av[i] = %s\n", av[i]);
+		pipex(av[1], av[ac - 1], &cmd, envp);
+		i++;
+		cmd.index--;
 	}
-	else if (cmd->index == cmd->last)
-	{
-		if (last_cmd(cmd, envp) == 0)
-			return (0);
-	}
-	else if ((cmd->index != cmd->first) || (cmd->index != cmd->last))
-	{
-		cmd->previous_fd = cmd->fd[0];
-		close(cmd->fd[1]);
-		pipe(cmd->fd);
-		if (middle_cmd(cmd, envp) == 0)
-			return (0);
-	}
-	return (1);
+	
 }
 
 int	main(int ac, char **av, char **envp)
@@ -115,7 +133,7 @@ int	main(int ac, char **av, char **envp)
 		return (0);
 	open_infile(&cmd, av[1]);
 	open_outfile(&cmd, av[ac - 1]);
-	cmd.nbr = ac - 3;
+	cmd.nbr_max = ac - 3;
 	cmd.index = cmd.nbr;
 	cmd.first = cmd.nbr;
 	cmd.last = 1;
@@ -123,19 +141,7 @@ int	main(int ac, char **av, char **envp)
 	cmd.pid = malloc(sizeof(int) * cmd.nbr);
 	if (!cmd.pid)
 		return (printf("pid error"), 0);
-	while (cmd.index > 0)
-	{
-		if (check_command(av[i], envp, &cmd) == 0)
-			return (0);
-		//dprintf(2, "cmd.nbr = %d\n", cmd.nbr);
-		//dprintf(2, "cmd.index = %d\n", cmd.index);
-		//dprintf(2, "i = %d\n", i);
-		//dprintf(2, "cmd path = %s\ncmd name = %s\n", cmd.path, cmd.name[0]);
-		//dprintf(2, "av[i] = %s\n", av[i]);
-		pipex(av[1], av[ac - 1], &cmd, envp);
-		i++;
-		cmd.index--;
-	}
+	
 	i = i - 2;
 	while (i > 0)
 	{
