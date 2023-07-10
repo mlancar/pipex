3b64dc747067043a0a08be9a5d2347eeac6fe7b2
mlancar <slmarine@hotmail.fr>
2023-07-10 02:50:20 +0200

    child function work

--- source/pipex.c
+++ source/pipex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: malancar <malancar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 16:27:53 by malancar          #+#    #+#             */
-/*   Updated: 2023/07/09 18:57:20 by malancar         ###   ########.fr       */
+/*   Updated: 2023/07/09 23:39:25 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ int	child(int fd_in, int fd_out, int fd_other, t_pipex *cmd, char **envp)
 	{
 		if ((dup2(fd_in, 0) != -1) && (dup2(fd_out, 1) != -1))
 		{
-			close(fd_other);
+			if (cmd->index != cmd->last)
+				close(fd_other);
 			if (execve(cmd->path, cmd->name, envp))
 				free_and_exit("execve", cmd);
 		}
@@ -30,8 +31,8 @@ int	child(int fd_in, int fd_out, int fd_other, t_pipex *cmd, char **envp)
 	}
 	else
 	{
-		close(fd_out);
 		close(fd_in);
+		close(fd_out);
 	}
 	return (1);
 }
@@ -56,7 +57,7 @@ int	here_doc(char *limiter, t_pipex *cmd, char **envp)
 	}//close
 	close(fd);
 	*/
-	fd = open("./tmp", O_WRONLY | O_CREAT, S_IRWXU);
+	fd = open("./tmp", O_WRONLY | O_CREAT, S_IWUSR);
 	if (fd == -1)
 		perror("open");
 	read_line = get_next_line(0);
@@ -70,74 +71,8 @@ int	here_doc(char *limiter, t_pipex *cmd, char **envp)
 	free(read_line);
 	close(fd);
 	fd = open("./tmp", O_RDONLY);
-	cmd->pid[cmd->index - 1] = fork();
-	if (cmd->pid[cmd->index - 1] < 0)
-		free_and_exit("fork", cmd);
-	else if (cmd->pid[cmd->index - 1] == 0)
-	{
-		if ((dup2(cmd->fd[1], 1) != -1) && (dup2(fd, 0) != -1))
-		{
-			close(cmd->fd[0]);
-			if (execve(cmd->path, cmd->name, envp))
-				free_and_exit("execve", cmd);
-		}
-		else
-			free_and_exit("dup2", cmd);
-	}
-	else
-	{
-		close(cmd->fd[1]);
-		close(fd);
-	}
-	return (1);
-}
-
-
-
-int	middle_cmd(t_pipex *cmd, char **envp)
-{
-	cmd->pid[cmd->index - 1] = fork();
-	if (cmd->pid[cmd->index - 1] < 0)
-		free_and_exit("fork", cmd);
-	if (cmd->pid[cmd->index - 1] == 0)
-	{
-		if ((dup2(cmd->previous_fd, 0) != -1) && (dup2(cmd->fd[1], 1) != -1))
-		{
-			close(cmd->fd[0]);
-			if (execve(cmd->path, cmd->name, envp))
-				free_and_exit("execve", cmd);
-		}
-		else
-			free_and_exit("dup2", cmd);
-	}
-	else
-	{
-		close(cmd->previous_fd);
-		close(cmd->fd[1]);
-	}
-	return (1);
-}
-
-int	last_cmd(t_pipex *cmd, char **envp)
-{
-	cmd->pid[cmd->index - 1] = fork();
-	if (cmd->pid[cmd->index - 1] < 0)
-		free_and_exit("fork", cmd);
-	if (cmd->pid[cmd->index - 1] == 0)
-	{
-		if ((dup2(cmd->fd[0], 0) != -1) && (dup2(cmd->outfile, 1) != -1))
-		{
-			if (execve(cmd->path, cmd->name, envp))
-				free_and_exit("execve", cmd);
-		}
-		else
-			free_and_exit("dup2", cmd);
-	}
-	else
-	{	
-		close(cmd->fd[0]);
-		close(cmd->outfile);
-	}
+	if (child(fd, cmd->fd[1], cmd->fd[0], cmd, envp) != 1)
+		free_and_exit("command fail", cmd);
 	return (1);
 }
 
@@ -163,18 +98,18 @@ void	pipex(t_pipex *cmd, char **av, char **envp)
 					free_and_exit("command fail", cmd);
 			}
 			else if (child(cmd->infile, cmd->fd[1], cmd->fd[0], cmd, envp) != 1)
-				free_and_exit("command fail", cmd);
+				free_and_exit("command fail first cmd", cmd);
 			
 		}
-		else if ((cmd->index == cmd->last) && (child(cmd->fd[0], cmd->outfile, cmd->fd[1], cmd, envp)))
-			free_and_exit("command fail", cmd);
+		else if ((cmd->index == cmd->last) && (child(cmd->fd[0], cmd->outfile, cmd->fd[1], cmd, envp) != 1))
+			free_and_exit("command fail last cmd", cmd);
 		else if ((cmd->index != cmd->first) && (cmd->index != cmd->last))
 		{
 			cmd->previous_fd = cmd->fd[0];
 			if (pipe(cmd->fd) == -1)
 				free_and_exit("pipe", cmd);
 			if (child(cmd->previous_fd, cmd->fd[1], cmd->fd[0], cmd, envp) == 0)
-				free_and_exit("command fail\n", cmd);
+				free_and_exit("command fail middle cmd", cmd);
 		}
 		i++;
 		cmd->index++;
